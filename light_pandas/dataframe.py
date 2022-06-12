fedfb5e6e87d75afc0a78ef2a37ef9f10074d337
bombhero <bomb.zhang@gmail.com>
2022-06-12 20:24:41 +0800

    Implemention is ongoing.

--- light_pandas/dataframe.py
+++ light_pandas/dataframe.py
@@ -1,6 +1,57 @@
 import copy
 
 
+class RowItem:
+    def __init__(self, df, row_idx):
+        self.df = df
+        self.row_idx = row_idx
+        self.current_col_idx = 0
+
+    def __setitem__(self, key, value):
+        col_idx = self.df.columns.index(key)
+        self.df.data_frame[self.row_idx][col_idx] = str(value)
+
+    def __iter__(self):
+        for idx in range(len(self)):
+            yield self.df.columns[idx], self.df.data_frame[self.row_idx][idx]
+
+    def __len__(self):
+        return len(self.df.columns)
+
+
+class IndexLocation:
+    def __init__(self, df):
+        self.df = df
+
+    def __getitem__(self, item):
+        if isinstance(item, slice):
+            result_df = DataFrame(columns=self.df.columns)
+            if item.start is None:
+                start_idx = 0
+            else:
+                start_idx = item.start
+            if item.stop is None:
+                stop_idx = len(self.df)
+            else:
+                stop_idx = item.stop
+            if item.step is None:
+                step_idx = 1
+            else:
+                step_idx = item.step
+            for row_idx in range(start_idx, stop_idx, step_idx):
+                row_dict = {}
+                for col_idx in range(len(self.df.columns)):
+                    row_dict[self.df.columns[col_idx]] = self.df.data_frame[row_idx][col_idx]
+                result_df = result_df.append(row_dict, ignore_index=True)
+            return result_df
+        elif isinstance(item, int):
+            if len(self.df.data_frame) > item:
+                row_item = RowItem(self.df, item)
+                return row_item
+            else:
+                raise IndexError("single positional indexer is out-of-bounds")
+
+
 class DataFrame:
     def __init__(self, columns=None):
         if columns is None:
@@ -8,6 +59,7 @@ class DataFrame:
         else:
             self.columns = [val for val in columns]
         self.data_frame = []
+        self.iloc = IndexLocation(self)
 
     def append(self, data_dict, ignore_index=False):
         df = copy.copy(self)
--- test/test_lpd.py
+++ test/test_lpd.py
@@ -15,8 +15,26 @@ class Test(unittest.TestCase):
 
     def test_read_csv(self):
         df = lpd.read_csv('test.csv')
-        self.assertEqual(len(df), 2)
-        self.assertListEqual(df.columns, ['item1', 'item2'])
+        self.assertEqual(len(df), 5)
+        self.assertListEqual(list(df.columns), ['item1', 'item2'])
+
+    def test_iloc(self):
+        df = lpd.read_csv('test.csv')
+        row_line = dict(df.iloc[1])
+        self.assertDictEqual(row_line, {'item1': 't3', 'item2': 't4'})
+
+    def test_iloc_mul(self):
+        df = lpd.read_csv('test.csv')
+        sub_df = df.iloc[1:3]
+        row_line = dict(sub_df.iloc[1])
+        self.assertEqual(len(sub_df), 2)
+        self.assertDictEqual(row_line, {'item1': 't5', 'item2': 't6'})
+
+    def test_iloc_set(self):
+        df = lpd.read_csv('test.csv')
+        df.iloc[1]['item1'] = 'bomb'
+        row_line = dict(df.iloc[1])
+        self.assertDictEqual(row_line, {'item1': 'bomb', 'item2': 't4'})
 
 
 if __name__ == '__main__':
