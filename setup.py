24c139d18b283a824905290d00b0c5739a9eaad9
bombhero <bomb.zhang@gmail.com>
2023-01-22 13:29:10 +0800

    Update the version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     setup(
         name='light-pandas',
         packages=find_packages(include=['lightpandas*']),
-        version='0.1.16',
+        version='0.1.17',
         description='Light weight Pandas library',
         long_description=long_description,
         long_description_content_type="text/markdown",
